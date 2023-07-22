cadcef0e2241565c2fdb257547e9022b44f24e21
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-07-22 16:24:55 +0500

    creating first pinia store

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TodoList from './TodoList.vue'
+import TodoForm from './TodoFrom.vue'
+</script>
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <todo-form></todo-form>
+    <todo-list></todo-list>
+  </div>
+</template>
+<style scoped></style>
